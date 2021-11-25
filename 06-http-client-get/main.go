ab1e698a35af78edc6031352ced9bf04f5494449
Vinay Jeurkar <vjeurkar03@gmail.com>
2021-11-25 20:51:59 +0530

    Updated HTTP client demo for GET and POST actions

--- 06-http-client-get/main.go
+++ 06-http-client-get/main.go
@@ -1,26 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
-	"net/url"
 )
 
+type Post struct {
+	UserId int    `json:"userId"`
+	Id     int    `json:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
+
 func main() {
-	resp, err := http.PostForm("https://jsonplaceholder.typicode.com/posts", url.Values{
-		"userId": {"1"},
-		"id":     {"1"},
-		"title":  {"My dummy Post"},
-		"body":   {"My dummy Post body"},
-	})
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
 	if err != nil {
-		log.Fatal("Unable to Post data")
+		log.Fatal("Unable to fetch data")
 	}
 
 	defer resp.Body.Close()
 	body, _ := io.ReadAll(resp.Body)
 
+	// String
 	fmt.Println(string(body))
+
+	// Struct
+	var jsonBody Post
+	json.NewDecoder(resp.Body).Decode(&jsonBody)
+	fmt.Println("UserId : ", jsonBody.UserId)
+	fmt.Println("Id : ", jsonBody.Id)
+	fmt.Println("Title : ", jsonBody.Title)
+	fmt.Println("Body : ", jsonBody.Body)
 }
